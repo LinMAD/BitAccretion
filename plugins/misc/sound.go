b13fd28be84c2849f4fc0cf8e97073606e09a0e2
Artjoms Nemiro <nemtjom@hotmail.com>
2018-08-22 22:52:19 +0200

    Add new sounds and play it randomly

--- plugins/misc/sound.go
+++ plugins/misc/sound.go
@@ -7,39 +7,62 @@ import (
 	"log"
 	"os"
 	"time"
+	"math/rand"
+	"io/ioutil"
 )
 
 const (
-	resourcePath = "/resources/sound/"
+	resourcePath = "/resources/sound"
+	voicePath    = resourcePath + "/voice/"
+	alarmPath    = resourcePath + "/alarm/"
 	sTag         = "SOUND"
 )
 
 // SendSoundAlert executes sound stream in speakers where runs program
 func SendSoundAlert() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	wd, err := os.Getwd()
 	if err != nil {
-		log.Printf(sTag + ": Could not retrieve working directory")
+		log.Printf(sTag + ": Errpr -> Could not retrieve working directory")
 
 		return
 	}
 
-	playAlert(wd)
-	playAttention(wd)
+	playAlert(wd + alarmPath)
+	playVoice(wd + voicePath)
+
 
 	return
 }
 
-// playAttention sound
-func playAttention(basePath string) {
-	attentionFile, _ := os.Open(basePath + resourcePath + "f_confirmcivilstatus_1_cut_spkr.wav")
-	alarmStream, format, _ := wav.Decode(attentionFile)
+// playVoice random voice file
+func playVoice(basePath string) {
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		log.Printf("%s: Error -> %v", sTag, err.Error())
 
-	play(alarmStream, format.SampleRate)
+		return
+	}
+
+	randVoice := files[rand.Intn(len(files))].Name()
+	voiceFile, _ := os.Open(basePath + randVoice)
+	voiceStream, format, _ := wav.Decode(voiceFile)
+
+	play(voiceStream, format.SampleRate)
 }
 
-// playAlert sound 3 times
+// playAlert random alert file
 func playAlert(basePath string) {
-	alarmFile, _ := os.Open(basePath + resourcePath + "manhack_alert_pass1_cut.wav")
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		log.Printf("%s: Error -> %v", sTag, err.Error())
+
+		return
+	}
+
+	randAlarm := files[rand.Intn(len(files))].Name()
+	alarmFile, _ := os.Open(basePath + randAlarm)
 	alarmStream, format, _ := wav.Decode(alarmFile)
 
 	play(alarmStream, format.SampleRate)
