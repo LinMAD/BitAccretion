c56a46a31727fff4d4af69c0a38dc6d915143c0b
Artjoms Nemiro <nemtjom@hotmail.com>
2019-06-07 11:07:52 +0200

    Add kernel to handle data updates and dashboard creation

---
+++ kernel/kernel.go
@@ -0,0 +1,146 @@
+package kernel
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/LinMAD/BitAccretion/dashboard"
+	"github.com/LinMAD/BitAccretion/event"
+	"github.com/LinMAD/BitAccretion/logger"
+	"github.com/LinMAD/BitAccretion/provider"
+	"github.com/mum4k/termdash"
+	"github.com/mum4k/termdash/terminal/terminalapi"
+)
+
+// Kernel core of whole application it's managing states and data communications
+type Kernel struct {
+	d *dashboard.MonitoringDashboard
+	p provider.IProvider
+	o event.IObserver
+	l logger.ILogger
+}
+
+// NewKernel of monitoring
+func NewKernel(dataProvider provider.IProvider) *Kernel {
+	k := &Kernel{
+		p: dataProvider,
+	}
+
+	return k
+}
+
+//initProvider for usages
+func (k *Kernel) initProvider(ctx context.Context) error {
+	wd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("could not retrieve working directory, error: %s", err.Error())
+	}
+
+	log.Println("Loading provider plugin configuration...")
+	pluginCfgErr := k.p.LoadConfig(wd + "/config.json")
+	if pluginCfgErr != nil {
+		return fmt.Errorf("provider configuration, error: %s", pluginCfgErr.Error())
+	}
+
+	log.Println("Preparing provider plugin...")
+	pluginBootErr := k.p.Boot()
+	if pluginBootErr != nil {
+		return fmt.Errorf("provider boot, error:: %s", pluginBootErr.Error())
+	}
+
+	return nil
+}
+
+// initDashboard to display monitored system data
+func (k *Kernel) initDashboard(ctx context.Context, t terminalapi.Terminal) error {
+	// TODO Can be added provider name to dashboard, interface update required
+	log.Println("Fetching first data from provider...")
+	providerGraph, providerGraphErr := k.p.DispatchMonitoredData()
+	if providerGraphErr != nil {
+		return fmt.Errorf("provider dispatch monitoring data failed, error: %s", providerGraphErr.Error())
+	}
+
+	log.Println("Creating terminal dashboard UI...")
+	var dErr error
+	k.d, dErr = dashboard.NewMonitoringDashboard("BitAccretion", t, providerGraph)
+	if dErr != nil {
+		return dErr
+	}
+
+	k.l = k.d.EventLogger
+	k.o = event.NewDashboardObserver(k.d.EventLogger)
+	k.o.RegisterNewSubscriber(k.d)
+
+	return nil
+}
+
+// dashboardUpdate ask provider to collect new data and push update to widgets
+func (k *Kernel) dashboardUpdate(ctx context.Context, delay time.Duration) {
+	log.Println("Executing background dashboard updates...")
+
+	ticker := time.NewTicker(delay)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			k.l.Normal("Requesting provider to get new data update...")
+
+			providerGraph, providerGraphErr := k.p.DispatchMonitoredData()
+			if providerGraphErr != nil {
+				k.l.Error(providerGraphErr.Error())
+				return
+			}
+
+			k.o.NotifySubscribers(event.UpdateEvent{MonitoringGraph: providerGraph})
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// Run main process to handle dashboard and update it with data from provider
+func (k *Kernel) Run(t terminalapi.Terminal) error {
+	log.Println("Initializing kernel...")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	providerErr := k.initProvider(ctx)
+	if providerErr != nil {
+		return providerErr
+	}
+
+	dashErr := k.initDashboard(ctx, t)
+	if dashErr != nil {
+		return dashErr
+	}
+	log.Println("Kernel ready...")
+
+	quitter := func(k *terminalapi.Keyboard) {
+		if k.Key == 'q' || k.Key == 'Q' {
+			cancel()
+		}
+	}
+
+	// TODO Time update must be used from Kernel cfg
+	go k.dashboardUpdate(ctx, 1*time.Second)
+
+	log.Println("Rendering terminal UI...")
+	fmt.Print("\033[H\033[2J") // Clean terminal screen from any artifacts
+
+	// TODO Time terminal UI redraw must be used from Kernel cfg
+	termErr := termdash.Run(
+		ctx,
+		t,
+		k.d.TerminalContainer,
+		termdash.KeyboardSubscriber(quitter),
+		termdash.RedrawInterval(1*time.Second),
+	)
+	if termErr != nil {
+		return termErr
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"os"
 	"plugin"
-	"time"
 
-	"github.com/LinMAD/BitAccretion/dashboard"
-	"github.com/LinMAD/BitAccretion/event"
-	"github.com/LinMAD/BitAccretion/logger"
-	"github.com/LinMAD/BitAccretion/model"
+	"github.com/LinMAD/BitAccretion/kernel"
 	"github.com/LinMAD/BitAccretion/provider"
-
-	"github.com/mum4k/termdash"
 	"github.com/mum4k/termdash/terminal/termbox"
-	"github.com/mum4k/termdash/terminal/terminalapi"
 )
 
+// TODO Refactor main, configuration and clean up
 var (
 	providerImpl provider.IProvider
 	configPath   string
@@ -53,98 +45,9 @@ func main() {
 	}
 	defer t.Close()
 
-	// TODO Move that to other place (Plugin warm up)
-	/**
-	PLUGIN WARM UP
-	*/
-
-	// Load plugin settings
-	pluginCfgErr := providerImpl.LoadConfig(configPath)
-	if pluginCfgErr != nil {
-		panic("Provider configuration, error: " + pluginCfgErr.Error())
-	}
-
-	// Boot plugin
-	pluginBootErr := providerImpl.Boot()
-	if pluginBootErr != nil {
-		panic("Provider boot, error: " + pluginBootErr.Error())
-	}
-
-	// Get first system graph
-	var providerGraph model.Graph
-	var providerGraphErr error
-	providerGraph, providerGraphErr = providerImpl.DispatchMonitoredData()
-	if providerGraphErr != nil {
-		panic("Provider dispatch monitoring data failed, error: " + providerGraphErr.Error())
-	}
-
-	// TODO Move that to other place
-	/**
-	Dashboard creation
-	*/
-
-	// TODO Use same context to cancel all widgets subscribers
-	ctx, cancel := context.WithCancel(context.Background())
-	monitoringDashboard, err := dashboard.NewMonitoringDashboard("BitAccretion", t, providerGraph)
-	if err != nil {
-		panic(err)
-	}
-
-	// TODO used ctx to cancel all widgets
-	monitoringObserver := event.NewDashboardObserver(monitoringDashboard.EventLogger)
-	monitoringObserver.RegisterNewSubscriber(monitoringDashboard)
-
-	quitter := func(k *terminalapi.Keyboard) {
-		if k.Key == 'q' || k.Key == 'Q' {
-			cancel()
-		}
-	}
-
-	/**
-	Observe data changes of system
-	*/
-	// TODO Move that to pckg
-	go providerMonitoring(monitoringObserver, monitoringDashboard.EventLogger, 1*time.Second)
-
-	/**
-	Handle dashboard view
-	*/
-	runErr := termdash.Run(
-		ctx,
-		t,
-		monitoringDashboard.TerminalContainer,
-		termdash.KeyboardSubscriber(quitter),
-		termdash.RedrawInterval(1*time.Second),
-	)
-	if runErr != nil {
-		panic(err)
-	}
-}
-
-// TODO Remove tmp func
-func providerMonitoring(monitoringObserver event.IObserver, log logger.ILogger, delay time.Duration) {
-	ticker := time.NewTicker(delay)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			log.Normal("Requesting for new monitoring graph update")
-
-			providerGraph, providerGraphErr := providerImpl.DispatchMonitoredData()
-			if providerGraphErr != nil {
-				log.Error(providerGraphErr.Error())
-				return
-			}
-
-			for _, n := range providerGraph.GetAllVertices() {
-				log.Normal(
-					fmt.Sprintf("New metrics of %s OK: %d ERR: %d", n.Name, n.Metric.RequestCount, n.Metric.ErrorCount),
-				)
-			}
-
-			monitoringObserver.NotifySubscribers(event.UpdateEvent{
-				MonitoringGraph: providerGraph,
-			})
-		}
+	k := kernel.NewKernel(providerImpl)
+	kErr := k.Run(t)
+	if kErr != nil {
+		panic(kErr)
 	}
 }
